3a49aa5e5455bbbb164097747ddaf47236c835da
GUILHERME ROSA LOPES <guilherme.rlopes6@senacsp.edu.br>
2025-02-06 22:18:20 -0300

    up 4

--- app.js
+++ app.js
@@ -5,5 +5,15 @@
  */
 
 function calcular() {
-    alert("teste do botao")
+    //capturar os valores das caixas de input
+    let gasolina = document.getElementById('gasolina').value
+    let etanol = document.getElementById('etanol').value
+    let kmGasolinagasolina = document.getElementById('kmGasolina').value
+    let kmEtanoltanol = document.getElementById('kmEtanol').value
+// calculo da vantagem
+if(etanol<(kmEtanoltanol/kmGasolinagasolina) *gasolina ) {
+    document.getElementById('status').src = 'img/etanol.png'
+} else {
+        document.getElementById('status').src = 'img/gasolina.png'
 }
+    }
